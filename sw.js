a3b7ff6c7f390dc8879234feee552d44c58a9ead
1223334444-abcd <yabukinat@gmail.com>
2026-07-04 15:37:11 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+{
+    "name": "Video Downloader",
+    "short_name": "VDown",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#667eea",
+    "theme_color": "#667eea",
+    "icons": [{
+        "src": "icon.png",
+        "sizes": "192x192",
+        "type": "image/png"
+    }]
+}
